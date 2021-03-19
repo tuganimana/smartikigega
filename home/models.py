e86d538033d14937365e275c01195c4396acce3c
Telesphore <tuganimana01@gmail.com>
2021-03-19 19:55:50 +0200

    added new changes sec

--- home/models.py
+++ home/models.py
@@ -35,8 +35,8 @@ class Active(models.Model):
     activate=models.FloatField(default=False)
     pub_date=models.DateTimeField(auto_now_add=True)
 class Farmers(models.Model):
-    firstname= models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
+    fullname= models.CharField(max_length=255)
+   
     gender = models.CharField(max_length=255)
     district = models.CharField(max_length=255)
     village = models.CharField(max_length=255)
@@ -45,8 +45,9 @@ class Farmers(models.Model):
     harvesttype = models.CharField(max_length=255)
     dateofbirth = models.CharField(max_length=255)
     number = models.CharField(max_length=255)
-    Cooperative=models.CharField(max_length=255)
+    Cooperative=models.CharField(max_length=255,default='')
     code=models.CharField(max_length=255)
+    pincode=models.CharField(max_length=255)
     sector=models.CharField(max_length=255) 
     cell=models.CharField(max_length=255) 
     def __str__(self):
