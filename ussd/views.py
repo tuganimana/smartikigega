020ec8ccf08762c9cecf943cce78859f70bee7c4
Telesphore <tuganimana01@gmail.com>
2021-03-22 21:03:49 +0200

    allnew

--- ussd/views.py
+++ ussd/views.py
@@ -225,7 +225,7 @@ def digitalapp(request):
         text = request.POST.get("text")
         level = text.split('*')
         response =""
-        numb = text[:3]
+        st = text[:3]
         farmers=Farmers.objects.all().filter(number=phone_number).order_by('-id')
         for users in farmers:
             phoneuser = users.number
